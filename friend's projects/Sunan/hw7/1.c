b633a7ce7bd25e22ff9e71693c62918db831b632
humam-hossain <humam.hossain.e@gmail.com>
2021-09-02 17:15:25 +0600

    updated on Thu 02 Sep 2021 05:15:25 PM +06

---
+++ friend's projects/Sunan/hw7/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main() {
+    char s1[80] = "A Journey of a Little Guy";
+    char s2[80] = "Thousand Miles";
+    char s3[80] = "Must begin now!";
+    char s4[80] = "with a Single Step.";
+    char s5[80];
+    char s6[80] = "";
+    strcpy(s5, "");
+    strncpy(s5, s1, 15);
+    strcat(s5, s2);
+    strcat(s5, " ");
+    strncat(s6, s3, 11);
+    strcat(s6, s4);
+    strcat(s5, s6);
+    printf("%s", s5);
+    return 0;
+}
