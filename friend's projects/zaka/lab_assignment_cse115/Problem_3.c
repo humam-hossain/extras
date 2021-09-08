37956fbb618739a4d4cf67d5433bd6dca841f900
humam-hossain <humam.hossain.e@gmail.com>
2021-09-09 02:32:02 +0600

    updated

--- friend's projects/zaka/lab_assignment_cse115/Problem_3.c
+++ friend's projects/zaka/lab_assignment_cse115/Problem_3.c
@@ -3,7 +3,10 @@ int N;
 
 typedef struct info
 { //Enter your code here
-
+    char Name[50];
+    char ID[50];
+    char Course[50];
+    char Section[50];
 }info;
 info S[100];
 
@@ -14,7 +17,9 @@ int main()
     scanf("%d",&N);
     fflush(stdin);
     //Enter your code here
+    while(fgets(S, sizeof(S), stdin)){
 
+    }
 
     return 0;
 }
--- friend's projects/zaka/lab_assignment_cse115/Problem_5.c
+++ friend's projects/zaka/lab_assignment_cse115/Problem_5.c
@@ -19,7 +19,10 @@ int main()
     int input1, input2, result;
     //GCD
     //Enter Code here
+    input1 = random();
+    input2 = random();
 
+    result = GCD(input1, input2);
 
     //---------------------
     printf("The GCD of %d and %d is %d", input1,input2,result);
@@ -27,13 +30,20 @@ int main()
     //DecToBin
     //Enter Code here
 
+    input1 = random();
 
+    result = DecToBin(input1);
+    printf("\n");
     //---------------------
     printf("The Binary of %d is %d", input1, result);
 
     //SeriesSum
     //Enter Code here
 
+    input1 = random();
+
+    result = SeriesSum(input1);
+    printf("\n");
 
     //---------------------
     printf("The Sum of series upto %d is %d", input1, result);
@@ -42,9 +52,10 @@ int main()
     //SequencePrint
     //Enter Code here
 
+    input1 = random();
+    printf("\n");
 
-
-
+    SequencePrint(input1);
 
     //---------------------
 
@@ -54,3 +65,46 @@ int main()
 
 //#####Enter your codes here#####
 
+int GCD(int a, int b) {
+    if (b != 0)
+        return GCD(b, a % b);
+    else
+        return a;
+}
+
+int DecToBin(int dec)
+{
+    if (dec == 0)
+        return 0;
+    else
+        return (dec % 2 + 10 * DecToBin(dec / 2));
+}
+
+int SeriesSum (int n){
+    static int sum = 1;
+
+    if(n == 0){
+        return sum;
+    }
+
+    sum = 1 + 0.5 * sum;
+
+    SeriesSum(n-1);
+}
+
+int SequencePrint (int n){
+    int tarr[1000];
+
+    tarr[0] = 0;
+    tarr[1] = 1;
+    tarr[2] = 0;
+
+    for(int i=0; i<n; i++){
+        if(i > 2){
+            tarr[i] = tarr[i-3] + tarr[i-2] + tarr[i-1];
+        }
+        printf("%d ", tarr[i]);
+    }
+
+    return 0;
+}
