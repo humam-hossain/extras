349486e5a9ce579422978009afd6aacccf05331b
humam-hossain <humam.hossain.e@gmail.com>
2021-09-09 00:43:22 +0600

    updated

--- friend's projects/zaka/lab_assignment_cse115/Problem_2.c
+++ friend's projects/zaka/lab_assignment_cse115/Problem_2.c
@@ -6,7 +6,16 @@ int main()
    int arr_1[10], arr_2[10]={0};
    printf("Enter 10 numbers for the array : \n");
    //Enter your code here
+    for(int i=0; i<10; i++){
+        scanf("%d", &arr_1[i]);
+    }
 
+    for(int i=9, j=0; i>=0; i--){
+        if(arr_1[i] % 2 != 0){
+            arr_2[j] = arr_1[i];
+            j++;
+        }
+    }
    //
    display(arr_1,arr_2);
     return 0;
