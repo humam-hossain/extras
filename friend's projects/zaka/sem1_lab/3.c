4518a4edd1437e601428e1fcefa6f7f3cf8666aa
humam-hossain <humam.hossain.e@gmail.com>
2021-09-18 12:06:57 +0600

    updated on Sat 18 Sep 2021 12:06:57 PM +06

--- friend's projects/zaka/sem1_lab/3.c
+++ friend's projects/zaka/sem1_lab/3.c
@@ -10,6 +10,9 @@ int main()
         {3,4,3,2,2,3},
     };
 
+    int position[2];
+    int y_position, x_position;
+
     for(int i=0; i<5; i++){
         for(int j=0; j<6; j++){
             printf("%d ", maze[i][j]);
@@ -17,4 +20,25 @@ int main()
         
         printf("\n");
     }
+
+    printf("Enter your postion: ");
+    scanf("%d %d", &position[0], &position[1]);
+
+    while(position[0] >=0 && position[0] <5 && position[1] >=0 && position[1] <6){
+        if(maze[position[0]][position[1]] == 1){
+            y_position = position[1];
+            position[1]++;
+        }else if(maze[position[0]][position[1]] == 2){
+            y_position = position[1];
+            position[1]--;
+        }else if(maze[position[0]][position[1]] == 3){
+            x_position = position[0];
+            position[0]--;
+        }else if(maze[position[0]][position[1]] == 4){
+            x_position = position[0];
+            position[0]++;
+        }
+    }
+
+    printf("%d %d", x_position, y_position);
 }
