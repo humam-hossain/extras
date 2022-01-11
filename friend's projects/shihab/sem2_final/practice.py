8c97d54797032e2417c94a4fc862c5420c5d4f4f
humam-hossain <humam.hossain.e@gmail.com>
2022-01-11 14:11:39 +0600

    updated on Tue Jan 11 02:11:38 PM +06 2022

---
+++ friend's projects/shihab/sem2_final/practice.py
@@ -0,0 +1,7 @@
+x= ("Dhaka Abdur Rahim", "Dhaka John Cena", "Narayanganj JohnWick", "Comilla CM Punk")
+
+print(x[0])
+
+# Output:
+# {'Dhaka': ['Dhaka Abdur Rahim', 'Dhaka John Cena'], 'Narayanganj':
+# ['Narayanganj John Wick'], 'Comilla': ['Comilla CM Punk']}
