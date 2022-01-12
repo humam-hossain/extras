2cf37a42b117b1dbca04526f7ff32744c26cfed0
humam-hossain <humam.hossain.e@gmail.com>
2022-01-12 11:13:16 +0600

    updated on Wed Jan 12 11:13:16 AM +06 2022

--- friend's projects/shihab/sem2_final/practice.py
+++ friend's projects/shihab/sem2_final/practice.py
@@ -1,21 +1,2 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-for _ in range(int(input())):
-    a = []
-    b = []
-    
-    input()
-    a = input().split()
-    input()
-    b = input().split()
-
-    result = True
-
-    for i in a:
-        if i not in b:
-            result = False
-    if result == True:
-        print(True)
-    else:
-        print(False)
+print(__name__)
     
