a20eecc7aceca7b673cdc203896d33c1f3a114b5
humam-hossain <humam.hossain.e@gmail.com>
2022-03-19 14:54:31 +0600

    updated on Sat Mar 19 02:54:31 PM +06 2022

---
+++ chorme_install.sh
@@ -0,0 +1,4 @@
+wget -O /home/painkiller/Downloads/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+cd /home/painkiller/Downloads
+apt install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
