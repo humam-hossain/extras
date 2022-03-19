aa4b7a75311d89f4212e330d3a629ffc05d79484
humam-hossain <humam.hossain.e@gmail.com>
2022-03-19 13:23:24 +0600

    updated on Sat Mar 19 01:23:24 PM +06 2022

---
+++ git_config.sh
@@ -0,0 +1,9 @@
+# github access token tutorial
+# https://www.youtube.com/watch?v=PMP3RmhkzkA
+
+git config --global user.name "humam-hossain"
+git config --global user.email "humam.hossain.e@gmail.com"
+
+# credential-helper
+git config --global credential.helper store
+git pull
